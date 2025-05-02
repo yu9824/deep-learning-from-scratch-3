77cd63790d66ee98d9f8b1cc7f67149536adc6f4
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-05-02 13:03:48 +0900

    step23

--- steps/step23.py
+++ steps/step23.py
@@ -1,12 +1,19 @@
 # %%
 # Add import path for the dezero directory.
-if '__file__' in globals():
-    import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import os
+import sys
+from pathlib import Path
+
+if "__file__" in globals():
+    sys.path.append(str((Path(__file__).parent / "..").resolve()))
+else:
+    sys.path.append(str(Path(os.getcwd(), "..").resolve()))
+
 
 # %%
 import numpy as np
-from dezero import Variable
+
+from dezero.core_simple import Variable
 
 
 # %%
