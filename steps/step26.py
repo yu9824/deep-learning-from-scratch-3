77cc15ba0817ed47a3ce4d0d3b4cf64bdbe11701
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-05-02 14:31:29 +0900

    step26

--- steps/step26.py
+++ steps/step26.py
@@ -1,19 +1,32 @@
 # %%
-'''
+"""
 Need the dot binary from the graphviz package (www.graphviz.org).
-'''
-if '__file__' in globals():
-    import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+"""
+import os
+import sys
+from pathlib import Path
+
+if "__file__" in globals():
+    sys.path.append(str((Path(__file__).parent / "..").resolve()))
+else:
+    sys.path.append(str(Path(os.getcwd(), "..").resolve()))
 import numpy as np
-from dezero import Variable
+
+from dezero.core_simple import Variable
 from dezero.utils import plot_dot_graph
 
 
 # %%
-def goldstein(x, y):
-    z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
-        (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+def goldstein(x: Variable, y: Variable) -> Variable:
+    z = (
+        1
+        + (x + y + 1) ** 2
+        * (19 - 14 * x + 3 * x**2 - 14 * y + 6 * x * y + 3 * y**2)
+    ) * (
+        30
+        + (2 * x - 3 * y) ** 2
+        * (18 - 32 * x + 12 * x**2 + 48 * y - 36 * x * y + 27 * y**2)
+    )
     return z
 
 
@@ -24,7 +37,7 @@ z = goldstein(x, y)
 z.backward()
 
 # %%
-x.name = 'x'
-y.name = 'y'
-z.name = 'z'
-plot_dot_graph(z, verbose=False, to_file='goldstein.png')
+x.name = "x"
+y.name = "y"
+z.name = "z"
+plot_dot_graph(z, verbose=False, to_file="goldstein.png")
