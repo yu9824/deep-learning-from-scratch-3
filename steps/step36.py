754bb8a4845ac3fb83a75758ad2d8ca7b896dd8a
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-05-17 19:25:35 +0900

    step36

--- steps/step36.py
+++ steps/step36.py
@@ -1,18 +1,24 @@
 # %%
-if '__file__' in globals():
-    import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import os
+import sys
+from pathlib import Path
+
 import numpy as np
+
+if "__file__" in globals():
+    sys.path.append(str((Path(__file__).parent / "..").resolve()))
+else:
+    sys.path.append(str(Path(os.getcwd(), "..").resolve()))
 from dezero import Variable
 
 # %%
 x = Variable(np.array(2.0))
-y = x ** 2
+y = x**2
 y.backward(create_graph=True)
 gx = x.grad
 x.cleargrad()
 
 # %%
-z = gx ** 3 + y
+z = gx**3 + y
 z.backward()
 print(x.grad)
